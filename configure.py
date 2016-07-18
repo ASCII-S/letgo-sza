bf243d983338c4259ab508e9a46aa489132150c5
Bo Fang <flyree@gmail.com>
2016-07-18 02:19:40 -0700

    add ./

--- configure.py
+++ configure.py
@@ -1,5 +1,5 @@
 pin_home = "/home/bo/pin-2.11-49306-gcc.3.4.6-ia32_intel64-linux/pin"
-benchmark = "lulesh.exe"
+benchmark = "./lulesh.exe"
 args = "5"
 toolbase = "/home/bo/pb_interceptor"
 pin_base = "/home/bo/pin-2.11-49306-gcc.3.4.6-ia32_intel64-linux/"
