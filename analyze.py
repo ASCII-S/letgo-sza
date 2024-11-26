85a8e9767a47200a1344ff6522ace4618b9c0813
tongshiyu <1600014464@qq.com>
2024-11-26 21:43:09 +0800

    nothing

--- analyze.py
+++ analyze.py
@@ -608,14 +608,14 @@ def extract_values_and_append_to_csv(input_file, log_dir, outputname, flag, sdc_
         if configure.progname == "miniFE":
             golden_bias = 4.45306e-06
             #默认最严格的sdc判定，如果误差小，就将sdc进化成masked
-            if abs(df.loc[0,'bias'] - golden_bias) < 10.0e-06:
+            if abs(float(df.loc[0,'bias']) - golden_bias) < 10.0e-06:
                 if df.loc[0,'result'] == 'SDC':
                      df.loc[0,'result'] = 'Masked'
                 if df.loc[0,'result'] == 'C-SDC':
                      df.loc[0,'result'] = 'C-Masked'
         if configure.progname == "HPCCG":
             golden_bias = 2.21357e-28
-            if abs(df.loc[0,'bias'] - golden_bias) > 10.0e-28:
+            if abs(float(df.loc[0,'bias']) - golden_bias) > 10.0e-28:
                 if df.loc[0,'result'] == 'SDC':
                      df.loc[0,'result'] = 'Masked'
                 if df.loc[0,'result'] == 'C-SDC':
